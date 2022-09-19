043df4bd5a576afea80b8c20514d6fe6deb5bb35
petitsarr <fayembad@gmail.com>
2022-09-19 14:36:33 +0000

    get_information_byform

--- public/script.js
+++ public/script.js
@@ -1,2 +1,30 @@
 "use strict";
-console.log("hello petit cva ");
+const form = document.getElementById("form");
+const type = document.getElementById("type");
+const firstname = document.getElementById("firstName");
+const lastname = document.getElementById("lastName");
+const address = document.getElementById("address");
+const country = document.getElementById("country");
+const town = document.getElementById("town");
+const zip = document.getElementById("zip");
+const product = document.getElementById("product");
+const price = document.getElementById("price");
+const quantity = document.getElementById("quantity");
+const tva = document.getElementById("tva");
+// Submit Form 
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(` 
+    ${type.value} 
+    ${firstname.value} 
+    ${lastname.value} 
+    ${address.value} 
+    ${country.value} 
+    ${town.value} 
+    ${zip.value} 
+    ${product.value} 
+    ${price.value} 
+    ${quantity.value} 
+    ${tva.value}
+    `);
+});
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,33 @@
-console.log("hello petit cva ")
+const form = document.getElementById("form") as HTMLFormElement 
+const type = document.getElementById("type" ) as HTMLSelectElement 
+const firstname  =  document.getElementById("firstName") as HTMLInputElement
+const lastname  =  document.getElementById("lastName") as HTMLInputElement 
+const address  =  document.getElementById("address") as HTMLInputElement 
+const country  =  document.getElementById("country") as HTMLInputElement 
+const town  =  document.getElementById("town") as HTMLInputElement 
+const zip   =  document.getElementById("zip") as HTMLInputElement 
+const product  =  document.getElementById("product") as HTMLInputElement 
+const price  =  document.getElementById("price") as HTMLInputElement 
+const quantity  =  document.getElementById("quantity") as HTMLInputElement 
+const tva =  document.getElementById("tva") as HTMLInputElement 
+
+
+// Submit Form 
+form.addEventListener("submit" , (e : Event)=>{ 
+    e.preventDefault() 
+
+    console.log(` 
+    ${type.value} 
+    ${firstname.value} 
+    ${lastname.value} 
+    ${address.value} 
+    ${country.value} 
+    ${town.value} 
+    ${zip.value} 
+    ${product.value} 
+    ${price.value} 
+    ${quantity.value} 
+    ${tva.value}
+    `)
+
+})
